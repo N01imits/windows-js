cbc8136dcb0864635f8f345865ee251aa44f9a0f
Ilya Belyakov <thesamecrazyman@gmail.com>
2023-11-28 12:49:05 +0300

    04_form_calculator_part1

--- src/modules/modals.js
+++ src/modules/modals.js
@@ -1,12 +1,24 @@
 const modals = () => {
-	function bindModal(triggerSelector, modalSelector, closeSelector) {
+	function bindModal({
+		triggerSelector,
+		modalSelector,
+		closeSelector,
+		closeClickOverlay = true,
+	}) {
 		const triggers = document.querySelectorAll(triggerSelector);
 		const modal = document.querySelector(modalSelector);
 		const close = document.querySelector(closeSelector);
+		const windows = document.querySelectorAll('[data-modal]');
 
 		const openModal = () => {
 			modal.style.display = 'block';
 			document.body.style.overflow = 'hidden';
+		};
+
+		const closeAllPopupWindows = () => {
+			windows.forEach(window => {
+				window.style.display = 'none';
+			});
 		};
 
 		const closeModal = () => {
@@ -19,14 +31,17 @@ const modals = () => {
 				if (e.target) {
 					e.preventDefault();
 				}
+				closeAllPopupWindows();
 				openModal();
 			});
 		});
 
 		close.addEventListener('click', closeModal);
+		close.addEventListener('click', closeAllPopupWindows);
 
 		modal.addEventListener('click', e => {
-			if (e.target === modal) {
+			if (e.target === modal && closeClickOverlay) {
+				closeAllPopupWindows();
 				closeModal();
 			}
 		});
@@ -45,12 +60,38 @@ const modals = () => {
 		}, time);
 	};
 
-	bindModal(
-		'.popup_engineer_btn',
-		'.popup_engineer',
-		'.popup_engineer .popup_close',
-	);
-	bindModal('.phone_link', '.popup', '.popup_close');
+	bindModal({
+		triggerSelector: '.popup_engineer_btn',
+		modalSelector: '.popup_engineer',
+		closeSelector: '.popup_engineer .popup_close',
+	});
+
+	bindModal({
+		triggerSelector: '.phone_link',
+		modalSelector: '.popup',
+		closeSelector: '.popup_close',
+	});
+
+	bindModal({
+		triggerSelector: '.popup_calc_btn',
+		modalSelector: '.popup_calc',
+		closeSelector: '.popup_calc_close',
+	});
+
+	bindModal({
+		triggerSelector: '.popup_calc_button',
+		modalSelector: '.popup_calc_profile',
+		closeSelector: '.popup_calc_profile_close',
+		closeClickOverlay: false,
+	});
+
+	bindModal({
+		triggerSelector: '.popup_calc_profile_button',
+		modalSelector: '.popup_calc_end',
+		closeSelector: '.popup_calc_end_close',
+		closeClickOverlay: false,
+	});
+
 	showModalByTime('.popup', 60000);
 };
 
